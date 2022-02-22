7c69ffcfec0463eec4b759cc8291d63982bdf317
Brando Rodriguez <brandorodriguez@gmail.com>
2022-02-22 00:06:57 -0500

    s

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function prev(){
   slides[index].classList.remove('active');
   index = (index - 1 + slides.length) % slides.length;
   slides[index].classList.add('active');
+  autoplay
 }
 
 let slide = document.querySelectorAll('.slide-containe');
